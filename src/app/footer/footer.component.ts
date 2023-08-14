183934daea92e37e08a4377ed7d37794a7a88382
Landvoigt <tim.voigt@ik.me>
2023-08-14 12:56:11 +0200

    linked projects and profiles

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,29 +6,6 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  @Input() showImprint: boolean | undefined;
-
-  gitUrl = 'https://github.com/Landvoigt';
-  linkedinUrl = 'https://github.com/Landvoigt';
-  emailUrl = 'https://github.com/Landvoigt';
-
-  toggleImprint() {
-    console.log('works');
-    this.showImprint = !this.showImprint;
-  }
-
-  redirectToGit() {
-    window.location.href = this.gitUrl;
-  }
-
-  redirectToEmail() {
-    window.location.href = this.emailUrl;
-  }
-
-  redirectToLinkedin() {
-    window.location.href = this.linkedinUrl;
-  }
-  
   scrollTop() {
     window.scroll({ top: 0, left: 0 });
   }
