2217359f96fb7dff447f4b79762cbbffc20282f0
Landvoigt <tim.voigt@ik.me>
2023-08-14 13:31:12 +0200

    updated anchor names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DescriptionComponent } from './description/description.component';
-import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { IntroductionComponent } from './introduction/introduction.component';
-import { ContactComponent } from './contact/contact.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 
 const routes: Routes = [
   { path: '', component: MainpageComponent },
-  { path: 'description', component: DescriptionComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'contact', component: ContactComponent },
   { path: 'imprint', component: ImprintComponent }
 ];
 
